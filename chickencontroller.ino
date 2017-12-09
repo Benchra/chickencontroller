bad4f31ccf7ea5d676609c30d26be3cf85590330
Benchra <Benchra@users.noreply.github.com>
2017-12-09 20:42:55 +0100

    Clock functionality updated + bugfixes
    time can now be set for both closing and opening times. Added logic and changed variables to accomodate the added functionality.
    closing times will still have the index 0 whilst opening timeswill use use index 1

--- chickencontroller.ino
+++ chickencontroller.ino
@@ -12,11 +12,13 @@
 #define btnLEFT   3
 #define btnSELECT 4
 #define btnNONE   5
+#define MAX_DISPLAY_LENGTH 15
+#define MIN_DISPLAY_LENGTH 0
 
 #include <avr/eeprom.h>
 #include <LiquidCrystal.h>
 
-boolean testrun = true;
+boolean testrun = false;
 
 const int rxPinIRFront = A7;
 const int txPinIRFront = 3;
@@ -26,12 +28,18 @@ const int VAL_DELAY = 25;
 const int VAL_TRIGGER_FRONT = -500;
 const int VAL_TRIGGER_BACK = 500;
 int clockArray[24][60];
-int hours = 23;
-int minutes = 30;
+int hoursClose = 23;
+int minutesClose = 30;
+int hoursOpen = 23;
+int minutesOpen = 30;
 boolean timeSet = false;
-int setHours = 0;
-int setMinutes = 0;
+int setHoursClose = 0;
+int setMinutesClose = 0;
+int setHoursOpen = 0;
+int setMinutesOpen = 0;
+int tempcursor = 0;
 
+//Statemachine variables
 int state = 0;
 int prevstate = 0;
 int logiccount = 0;
@@ -365,7 +373,29 @@ void loop() {
     lcd.print("Chicken inside");
   }
   else{
-    lcd.print("Set Closing Time:");
+    if(!timeSet)
+    {
+     if (cursorposition >= 0 && cursorposition <= 3)
+     {
+      lcd.print("Set Closing H  ");
+     }
+     else if (cursorposition > 3 && cursorposition <= 7)
+     {
+      lcd.print("Set Closing Min  ");
+     }
+     else if (cursorposition > 7 && cursorposition <= 11)
+     {
+      lcd.print("Set Opening H  ");
+     }
+     else if (cursorposition > 11 && cursorposition <= 15)
+     {
+      lcd.print("Set Opening Min  ");
+     }
+    }
+    else
+    {
+      lcd.print("Close/Open Time:  ");
+    }
   }
   
   //lcd.setCursor(7,1);            // move to the begining of the second line
@@ -387,8 +417,8 @@ int read_LCD_buttons()
  if (adc_key_in < 120)   return btnRIGHT;  
  if (adc_key_in < 230)  return btnUP; 
  if (adc_key_in < 350)  return btnDOWN; 
- if (adc_key_in < 460)  return btnLEFT; 
- if (adc_key_in < 670)  return btnSELECT;   
+ if (adc_key_in < 500)  return btnLEFT; 
+ if (adc_key_in < 700)  return btnSELECT;   
  
  return btnNONE;  // default value
 }
@@ -396,18 +426,18 @@ int read_LCD_buttons()
 void keytrigger()
 {
  lcd.setCursor(0,1);
- if(cursorposition < 0) {cursorposition = 0;}
- if(cursorposition >= 4) {cursorposition = 4;}
- switch (lcd_key)               // depending on which button was pushed, we perform an action
+ if(cursorposition < MIN_DISPLAY_LENGTH) {cursorposition = MIN_DISPLAY_LENGTH;}
+ if(cursorposition >= MAX_DISPLAY_LENGTH) {cursorposition = MAX_DISPLAY_LENGTH;}
+ switch (lcd_key)               // depending on which button was pushed, perform an action
  {
    case btnRIGHT:
      {
       clockChanged = true;
      //change cursor to right position unless already to the rightmost position
-     lcd.print("Cursor to RIGHT");
+     //lcd.print("Cursor to RIGHT");
      lcd.setCursor(cursorposition,1);
      cursorposition++; 
-     if(cursorposition >= 4) {cursorposition = 4;}
+     if(cursorposition >= MAX_DISPLAY_LENGTH) {cursorposition = MAX_DISPLAY_LENGTH;}
      lcd.setCursor(cursorposition,1);
      break;
      }
@@ -415,61 +445,89 @@ void keytrigger()
      {
       clockChanged = true;
      //change cursor to left position unless already to the leftmost position
-     lcd.print("Cursor to LEFT");
+     //lcd.print("Cursor to LEFT");
      lcd.setCursor(cursorposition,1);
      cursorposition--; 
-     if(cursorposition < 0) {cursorposition = 0;}
+     if(cursorposition < MIN_DISPLAY_LENGTH) {cursorposition = MIN_DISPLAY_LENGTH;}
      lcd.setCursor(cursorposition,1);
      break;
      }
    case btnUP:
      {
-     clockChanged = true;
-     //Increment clock value at selected position
-     lcd.setCursor(cursorposition,1);
-     if (cursorposition == 0 || cursorposition == 1)
+     if(!timeSet)
      {
-      hours++;
+       clockChanged = true;
+       //Increment clock value at selected position
+       lcd.setCursor(cursorposition,1);
+       if (cursorposition >= 0 && cursorposition <= 3)
+       {
+        hoursClose++;
+       }
+       else if (cursorposition > 3 && cursorposition <= 7)
+       {
+        minutesClose++;
+       }
+       else if (cursorposition > 7 && cursorposition <= 11)
+       {
+        hoursOpen++;
+       }
+       else if (cursorposition > 11 && cursorposition <= 15)
+       {
+        minutesOpen++;
+       }
+       setClockValues();
      }
-     if (cursorposition == 3 || cursorposition == 4)
-     {
-      minutes++;
-     }
-     setClockValues();
      break;
      }
    case btnDOWN:
      {
-     clockChanged = true;
-     //Decrement clock value at selected position
-     lcd.setCursor(cursorposition,1);
-     if (cursorposition == 0 || cursorposition == 1)
+     if(!timeSet)
      {
-      hours--;
+       clockChanged = true;
+       //Decrement clock value at selected position
+       lcd.setCursor(cursorposition,1);
+       if (cursorposition >= 0 && cursorposition <= 3)
+       {
+        hoursClose--;
+       }
+       else if (cursorposition > 3 && cursorposition <= 7)
+       {
+        minutesClose--;
+       }
+       else if (cursorposition > 7 && cursorposition <= 11)
+       {
+        hoursOpen--;
+       }
+       else if (cursorposition > 11 && cursorposition <= 15)
+       {
+        minutesOpen--;
+       }
+       setClockValues();
      }
-     if (cursorposition == 3 || cursorposition == 4)
-     {
-      minutes--;
-     }
-     setClockValues();
      break;
      }
    case btnSELECT:
      {
       //TODO Set clock value for all positions
      clockChanged = true;
-     lcd.print("SELECT");
+     //lcd.print("SELECT");
      //
      if(timeSet){
-       setHours = 0;
-       setMinutes = 0;
+       setHoursClose = 0;
+       setMinutesClose = 0;
+       setHoursOpen = 0;
+       setMinutesOpen = 0;
        timeSet = false;
+       delay(1000);
      }
      else
      {
-       setHours = clockArray[hours][0];
-       setMinutes = clockArray[0][minutes];
+       setHoursClose = clockArray[hoursClose][0];
+       setMinutesClose = clockArray[0][minutesClose];
+       setHoursOpen = clockArray[hoursClose][1];
+       setMinutesOpen = clockArray[1][minutesClose];
        timeSet = true;
+       delay(1000);
      }
      break;
      }
@@ -488,40 +546,118 @@ void keytrigger()
 
 void setClockValues(){
      //Correcting faulty values
-     if(minutes > 59){ hours++; minutes = 0;}
-     if(minutes < 0) { hours--; minutes =59; }
-     if(hours > 23){ hours = 0;}
-     if(hours < 0) { hours = 23; }
-
-     //Setting Values
-     lcd.setCursor(0,1);
-     if(hours < 10 && hours > -1){lcd.print("0"); lcd.setCursor(1,1);}
-     lcd.print(clockArray[hours][0]);
-     lcd.setCursor(2,1);
-     lcd.print(":");
-     lcd.setCursor(3,1);
-     if(minutes < 10){lcd.print("0"); lcd.setCursor(4,1);}
-     lcd.print(clockArray[0][minutes]);
-
-     //CLEAR DISPLAY AFTER TIME
-     lcd.setCursor(5,1);
-     lcd.print("           ");
+     if(minutesClose > 59){ hoursClose++; minutesClose = 0;}
+     if(minutesClose < 0) { hoursClose--; minutesClose = 59; }
+     if(hoursClose > 23){ hoursClose = 0;}
+     if(hoursClose < 0) { hoursClose = 23; }
      
-     //Display Cursorposition
-     lcd.setCursor(8,1);
-     lcd.print(cursorposition);
+     if(minutesOpen > 59){ hoursOpen++; minutesOpen = 0;}
+     if(minutesOpen < 0) { hoursOpen--; minutesOpen = 59; }
+     if(hoursOpen > 23){ hoursOpen = 0;}
+     if(hoursOpen < 0) { hoursOpen = 23; }
+
+     tempcursor = 0;
+     
+     /*SETTING VALUES CLOSING TIME */
+     lcd.setCursor(tempcursor,1);
+     if(hoursClose < 10 && hoursClose > -1)
+     {
+      lcd.print("0"); 
+      tempcursor++; 
+      lcd.setCursor(tempcursor,1);
+      lcd.print(clockArray[hoursClose][0]);
+      tempcursor++;
+     }
+     else
+     {
+      lcd.print(clockArray[hoursClose][0]);
+      tempcursor += 2;
+     }
+     lcd.setCursor(tempcursor,1);
+     lcd.print(":");
+     tempcursor++;
+     lcd.setCursor(tempcursor,1);
+     if(minutesClose < 10)
+     {
+      lcd.print("0"); 
+      tempcursor++; 
+      lcd.setCursor(tempcursor,1);
+      lcd.print(clockArray[0][minutesClose]);
+      tempcursor++;
+     }
+     else
+     {
+      lcd.print(clockArray[0][minutesClose]); 
+      tempcursor += 2;
+     }
+     //tempcursor at +5 higher than starting value now
+
+    
+    lcd.print("cl"); 
+    tempcursor += 2;
+
+    
+    lcd.print("  "); 
+    tempcursor += 2;
+
+    /*SETTING VALUES OPENING TIME */
+     lcd.setCursor(tempcursor,1);
+     if(hoursOpen < 10 && hoursOpen > -1)
+     {
+      lcd.print("0"); 
+      tempcursor++; 
+      lcd.setCursor(tempcursor,1);
+      lcd.print(clockArray[hoursOpen][1]);
+      tempcursor++;
+     }
+     else
+     {
+      lcd.print(clockArray[hoursOpen][1]);
+      tempcursor += 2;
+     }
+     lcd.setCursor(tempcursor,1);
+     lcd.print(":");
+     tempcursor++;
+     lcd.setCursor(tempcursor,1);
+     if(minutesOpen < 10)
+     {
+      lcd.print("0"); 
+      tempcursor++; 
+      lcd.setCursor(tempcursor,1);
+      lcd.print(clockArray[1][minutesOpen]);
+      tempcursor++;
+     }
+     else
+     {
+      lcd.print(clockArray[1][minutesOpen]); 
+      tempcursor += 2;
+     }
+     //tempcursor at +5 higher than starting value now
+     
+     lcd.print("op"); 
+     tempcursor += 2;
 }
 
 void correctingInitialClockValues(){
-   if(minutes == 60) { minutes = 0;}
-   if(hours == 60) { hours = 0;}
+   if(minutesClose == 60) { minutesClose = 0;}
+   if(hoursClose == 60) { hoursClose = 0;}
+   if(minutesOpen == 60) { minutesOpen = 0;}
+   if(hoursOpen == 60) { hoursOpen = 0;}
 }
 
 void initClockArray(){
+  //index 0 == closing time
   for(int h = 0; h < 24; h++){
       clockArray[h][0] = h;
   }
   for(int m = 0; m < 60; m++){
       clockArray[0][m] = m;
+  }
+  //index 1 == opening time
+  for(int h = 0; h < 24; h++){
+      clockArray[h][1] = h;
+  }
+  for(int m = 0; m < 60; m++){
+      clockArray[1][m] = m;
   }
 }
